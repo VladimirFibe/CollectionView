5fd93e2e60e40af6ed367bdc153968eadee695f8
Vladimir <motiw@icloud.com>
2022-06-29 14:06:11 +0600

    Start

---
+++ CollectionView/NumberCell.swift
@@ -0,0 +1,14 @@
+//
+//  NumberCell.swift
+//  CollectionView
+//
+//  Created by Vladimir Fibe on 29.06.2022.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+  @IBOutlet weak var label: UILabel!
+  static let reuseIdentifier = String(describing: NumberCell.self)
+  
+}
