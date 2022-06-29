3d71555b5b6c52e00388e347b2d16c3165c7706f
Vladimir <motiw@icloud.com>
2022-06-29 15:13:54 +0600

    Lesson 06

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -9,14 +9,20 @@ import UIKit
 import SwiftUI
 
 class ViewController: UIViewController {
-
+  
+  enum Section {
+    case main
+    
+  }
+  var dataSource: UICollectionViewDiffableDataSource<Section, Int>!
   @IBOutlet weak var collectionView: UICollectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     collectionView.collectionViewLayout = configureLayout()
+    configureDataSource()
   }
-   
+  
   func configureLayout() -> UICollectionViewCompositionalLayout {
     let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
     let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -25,6 +31,36 @@ class ViewController: UIViewController {
     let section = NSCollectionLayoutSection(group: group)
     return UICollectionViewCompositionalLayout(section: section)
   }
-
+  
+  func configureDataSource() {
+    dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: self.collectionView) { (collectionView, indexPath, number) -> UICollectionViewCell? in
+      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCell.reuseIdentifier, for: indexPath) as? NumberCell else {
+        fatalError("Cannot create new cell")
+      }
+      cell.label.text = number.description
+      return cell
+    }
+    var initialSnapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+    initialSnapshot.appendSections([.main])
+    initialSnapshot.appendItems(Array(1...100))
+    dataSource.apply(initialSnapshot, animatingDifferences: false)
+  }
 }
 
+// MARK: - Preview
+
+struct ViewControllerSUI: UIViewControllerRepresentable {
+  func makeUIViewController(context: Context) -> ViewController {
+    let viewController = ViewController()
+    return viewController
+  }
+  
+  func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    
+  }
+}
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    Text("Kill Storyboard!")
+  }
+}
